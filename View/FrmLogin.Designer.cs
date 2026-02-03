05023845de26e76725f26812d3b94fdadf6dec49
Edson Rodrigues <edsonrb.2008@gmail.com>
2026-02-03 16:29:14 -0300

    Programação do Login

---
+++ View/FrmLogin.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace SistemaAtendimento.View
+{
+    partial class FrmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtEmail = new TextBox();
+            txtSenha = new TextBox();
+            btnEntrar = new Button();
+            SuspendLayout();
+            // 
+            // txtEmail
+            // 
+            txtEmail.Location = new Point(83, 45);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(170, 23);
+            txtEmail.TabIndex = 0;
+            // 
+            // txtSenha
+            // 
+            txtSenha.Location = new Point(83, 94);
+            txtSenha.Name = "txtSenha";
+            txtSenha.Size = new Size(170, 23);
+            txtSenha.TabIndex = 1;
+            // 
+            // btnEntrar
+            // 
+            btnEntrar.Location = new Point(123, 155);
+            btnEntrar.Name = "btnEntrar";
+            btnEntrar.Size = new Size(75, 23);
+            btnEntrar.TabIndex = 2;
+            btnEntrar.Text = "button1";
+            btnEntrar.UseVisualStyleBackColor = true;
+            btnEntrar.Click += btnEntrar_Click;
+            // 
+            // FrmLogin
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(397, 259);
+            Controls.Add(btnEntrar);
+            Controls.Add(txtSenha);
+            Controls.Add(txtEmail);
+            Name = "FrmLogin";
+            Text = "Login";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtEmail;
+        private TextBox txtSenha;
+        private Button btnEntrar;
+    }
+}
---
+++ View/FrmLogin.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SistemaAtendimento.Controller;
+using SistemaAtendimento.Model;
+
+namespace SistemaAtendimento.View
+{
+    public partial class FrmLogin : Form
+    {
+
+        private UsuarioController _usuarioController;
+        public FrmLogin()
+        {
+
+            InitializeComponent();
+            _usuarioController = new UsuarioController(null);
+        }
+
+        private void btnEntrar_Click(object sender, EventArgs e)
+        {
+            
+
+            Usuarios usuario = _usuarioController.Autenticar(
+                txtEmail.Text.Trim(),
+                txtSenha.Text.Trim()
+            );
+
+            if (usuario != null)
+            {
+                MessageBox.Show($"Bem-vindo, {usuario.Nome}!");
+
+                FrmTelaPrincipal frm = new FrmTelaPrincipal();
+                frm.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Email ou senha inválidos.",
+                    "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
