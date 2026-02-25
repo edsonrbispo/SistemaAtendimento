5fed9a80afaea2df9d58344110965614f3e3de9b
Edson Rodrigues <edsonrb.2008@gmail.com>
2026-02-25 19:43:03 -0300

    Relatorio

--- Services/RelatorioClientes.cs
+++ Services/RelatorioClientes.cs
@@ -24,13 +24,38 @@ namespace SistemaAtendimento.Services
                     page.DefaultTextStyle(x=> x.FontSize(10).FontFamily(Fonts.Verdana));
 
                     //Início Cabeçalho
-                    page.Header().BorderBottom(1).PaddingBottom(5).Row(row => { });
+                    page.Header().BorderBottom(1).PaddingBottom(10).Row(row => {
+
+                        row.RelativeItem(1).Column(col => {
+                            string logoPath = Path.Combine(AppContext.BaseDirectory,"Assets","logotiposenac.png");
+                            if (File.Exists(logoPath))
+                            {
+                                col.Item().Width(80).Image(logoPath);
+                            }
+                            else
+                            {
+                                col.Item().Text("SENAC").FontSize(14).Bold();
+                            }
+                        });
+
+                        row.RelativeItem(1).AlignCenter().AlignMiddle().Text("Lista de Clientes").FontSize(16).Bold();
+
+                        row.RelativeItem(1).AlignRight().AlignMiddle().Text(t => { 
+                            t.Span("Data: ").Bold();
+                            t.Span(DateTime.Now.ToString("dd/MM/yyyy"));                          
+                        });
+                    
+                    });
 
                     //Conteúdo Central
-                    page.Content().PaddingVertical(10).Table(table => { });
+                    //page.Content().PaddingVertical(10).Table(table => { });
+
 
                     //Rodapé
-                    page.Footer().AlignCenter().Text(t => { });
+                    page.Footer().AlignCenter().Text(t => {
+                        t.Span("Página ");
+                        t.CurrentPageNumber();
+                    });
 
                 });
             
